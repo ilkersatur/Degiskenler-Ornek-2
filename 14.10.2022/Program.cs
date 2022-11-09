b828e259edbd7807ccbf6e41969d012b2a524cf2
İlker <ilkersatur@gmail.com>
2022-11-09 23:30:09 +0300

    Add project files.

---
+++ 14.10.2022/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+
+int sayi1 = 15;
+object obj1 = sayi1;
+//obj1 = obj1 * 3; çarpılmıyor
+int unbox = (int)obj1;
+unbox = unbox * 3;
+
+////////////////////////
+
+string metin = "ilker şatur";
+object obj2 = metin;
+string str = (string)obj2;
+str.Split(' ');
+
+
+///////////////////////
+
+// OBJELERİ ÇARPMA
+
+int id = 15;
+
+object masa_genislik =120;
+object masa_yukseklik= 100;
+
+int d = (int)masa_genislik;
+
+int h = (int)masa_yukseklik;
+
+int alan = d * h;
+
+Console.WriteLine("ID= "+ id+" alan "+alan); */
+
+Console.Write("tahta genişlik ");
+object tahta_genislik = /*int.Parse(Console.ReadLine());*/  Convert.ToInt16(Console.ReadLine());
+Console.Write("tahta yükseklik ");
+object tahta_yukseklik = int.Parse(Console.ReadLine());
+
+
+int alan = (int)tahta_genislik * (int)tahta_yukseklik; ;
+
+///////////////
+
+Console.Write("genişlik ");
+object genislik = int.Parse(Console.ReadLine());
+Console.Write("yükseklik ");
+object yukseklik = int.Parse(Console.ReadLine());
+
+
+int alan2 = (int)genislik * (int)yukseklik;
+
+///////////////
+
+Console.WriteLine("siyah alan " + (alan - alan2));
---
+++ Ornek/Program.cs
@@ -0,0 +1,58 @@
+﻿class Masa
+{
+    public int id;
+    public string renk;
+    public int tip;
+    public int w;
+    public int h;
+    public int d;
+    public double r;
+    public string tipi;
+    public double masa_alan;
+    public double masa_hacim;
+    public double pi = Math.PI;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Masa masa1 = new Masa();
+        Console.Write("id= ");
+        masa1.id = Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("renk= ");
+        masa1.renk = Convert.ToString(Console.ReadLine());
+
+        Console.Write("yuvarlak ise 1 kare ise 0 ");
+        masa1.tip = Convert.ToInt16(Console.ReadLine());
+        if (masa1.tip == 0)
+        {
+            Console.Write("d=");
+            masa1.w = Convert.ToInt16(Console.ReadLine());
+            Console.Write("w=");
+            masa1.h = Convert.ToInt16(Console.ReadLine());
+            Console.Write("h=");
+            masa1.d = Convert.ToInt16(Console.ReadLine());
+            masa1.tipi = "kare";
+            masa1.masa_alan = masa1.w * masa1.h;
+            masa1.masa_hacim = masa1.masa_alan * masa1.d;
+        }
+        else if (masa1.tip == 1)
+        {
+            Console.Write("r=");
+            masa1.r = Convert.ToInt16(Console.ReadLine());
+            Console.Write("h=");
+            masa1.d = Convert.ToInt16(Console.ReadLine());
+            masa1.masa_alan = masa1.r * masa1.r * masa1.pi;
+            masa1.masa_hacim = masa1.masa_alan * masa1.d;
+            masa1.tipi = "yuvarlak";
+        }
+        else
+        {
+            Console.WriteLine("1 ya da 0 gir");
+        }
+        Console.WriteLine(masa1.id + " " + masa1.tipi + " " + masa1.renk + " masa " + " alan= " + masa1.masa_alan + " hacim= " + masa1.masa_hacim);
+        Console.WriteLine("");
+        Console.WriteLine("");
+    }
+}
